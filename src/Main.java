83f0f57d5774dd61d5e9de36bdbf738321e7c66f
SalomãoTK96 <patrick26franca@hotmail.com>
2026-06-26 20:49:57 -0300

    comentarios e cabecalho

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Sistema de Passagens de Ônibus - Catarinense
+ * Disciplina: Banco de Dados - UNIVALI
+ * Autores: Salomão Patrick França Alves Panas, João Vitor
+ */
 
 import menu.MenuPrincipal;
 
